a99f1f0b7cce639e5d16d71908a78d04ad7fe435
Shedrack Okunda <okundashedrack@gmail.com>
2024-12-02 21:07:26 +0300

    created a password reset token model

---
+++ models/PasswordResetToken.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const passwordResetTokenSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  token: { type: String, required: true, unique: true },
+  expiresAt: { type: Date, required: true },
+});
+
+const PasswordResetToken = mongoose.model(
+  "PasswordResetToken",
+  passwordResetTokenSchema,
+);
+
+export default PasswordResetToken;
