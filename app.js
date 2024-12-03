750de1bdf2d2691095a29dd0ed1d2507f8780329
Shedrack Okunda <okundashedrack@gmail.com>
2024-12-03 17:46:27 +0300

    added routes

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoute.js";
 
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
 const db = process.env.DB;
-
-app.use(cors());
-app.use(express.json());
 
 // db connection
 mongoose
@@ -21,6 +23,27 @@ mongoose
     console.log("Error:", error);
   });
 
-app.listen(8080, () => {
+// middlewares
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+    exposedHeaders: ["X-Total-Count"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("tiny"));
+
+// route middleware
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "running" });
+});
+
+app.listen(port, () => {
   console.log("Server running on http://localhost:8080");
 });
